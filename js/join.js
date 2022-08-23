bf614bad49b70c94488b3c6b70ce851452e3da2e
jm011jh <jm011jh@gmail.com>
2022-08-23 21:57:52 +0900

    join-ing

--- js/join.js
+++ js/join.js
@@ -26,3 +26,98 @@ pwSwitchButtonRe.addEventListener("click",function(){
         pwSwRe = false
     }
 })
+
+// open button
+const openEmailList = document.getElementById("openEmailList")
+const openCoEmailList = document.getElementById("openCoEmailList")
+
+
+// select box
+const emailList = document.getElementById("emailList")
+const coEmailList = document.getElementById("coEmailList")
+const yearList = document.getElementById("yearList")
+const classList = document.getElementById("classList")
+
+// select options
+const emailListItem = emailList.querySelectorAll(".email-list-item")
+const coEmailListItem = coEmailList.querySelectorAll(".co-email-list-item")
+const yearListItem = yearList.querySelectorAll(".year-list-item")
+const classListItem = classList.querySelectorAll(".class-list-item")
+
+
+// input
+const chargerEmail = document.getElementById("chargerEmail")
+const coEmail = document.getElementById("coEmail")
+const coYear = document.getElementById("coYear")
+const doClass = document.getElementById("doClass")
+
+openEmailList.addEventListener('click',function(){
+    emailList.classList.add("on")
+})
+openCoEmailList.addEventListener('click',function(){
+    coEmailList.classList.add("on")
+})
+coYear.addEventListener('click',function(){
+    yearList.classList.add("on")
+})
+doClass.addEventListener('click',function(){
+    classList.classList.add("on")
+})
+emailListItem.forEach(function(el){
+    el.addEventListener("click",function(){
+        let text = el.innerHTML;
+        chargerEmail.value = text
+        if(text === "직접입력"){
+            chargerEmail.value = ''
+        }else{
+        }
+        emailList.classList.remove("on")
+        openEmailList.querySelector('p').innerHTML = ''
+        openEmailList.querySelector('p').innerHTML = text
+    })
+})
+coEmailListItem.forEach(function(el){
+    el.addEventListener("click",function(){
+        let text = el.innerHTML;
+        coEmail.value = text
+        if(text === "직접입력"){
+            coEmail.value = ''
+        }else{
+        }
+        coEmailList.classList.remove("on")
+        openCoEmailList.querySelector('p').innerHTML = ''
+        openCoEmailList.querySelector('p').innerHTML = text
+    })
+})
+yearListItem.forEach(function(el){
+    el.addEventListener("click",function(){
+        console.log('hi')
+        let text = el.innerHTML;
+        coYear.value = text
+        coYear.placeholder = text
+        if(text === "직접입력"){
+            coYear.value = ''
+            coYear.placeholder = ''
+        }else{
+        }
+        yearList.classList.remove("on")
+        openYearList.querySelector('p').innerHTML = ''
+        openYearList.querySelector('p').innerHTML = text
+    })
+})
+classListItem.forEach(function(el){
+    el.addEventListener("click",function(){
+        console.log('hi')
+        let text = el.innerHTML;
+        doClass.value = text
+        doClass.placeholder = text
+        if(text === "직접입력"){
+            doClass.value = ''
+            doClass.placeholder = ''
+        }else{
+        }
+        classList.classList.remove("on")
+        openclassList.querySelector('p').innerHTML = ''
+        openclassList.querySelector('p').innerHTML = text
+    })
+})
--- js/member.js
+++ js/member.js
@@ -1,9 +1,11 @@
 const inputBox = document.querySelectorAll(".input-box")
 
 inputBox.forEach(function(el,_i){
+    console.log(el)
     let input = el.querySelector("input")
     let parent = el.parentElement;
     input.addEventListener("focus",function(){
+        console.log("focused")
         parent.classList.remove("error-now")
         for(a of inputBox){
             a.classList.remove("focused")
